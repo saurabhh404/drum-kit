3f48229335373842ffea396380484839b1b535c9
Saurabh Sutar <64704574+saurabhh404@users.noreply.github.com>
2020-05-23 14:53:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    // this.style.color = "white";
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+  });
+}
+
+
+document.addEventListener("keydown", function() {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "j":
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "k":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+    case "l":
+      new Audio("sounds/snare.mp3").play();
+      break;
+  }
+}
+
+function buttonAnimation(key) {
+//document.querySelector("h1").classList.add("huge");
+ var activeButton = document.querySelector("." + key);
+ activeButton.classList.add("pressed");
+ setTimeout(function(){
+   activeButton.classList.remove("pressed");
+ }, 100);
+
+}
